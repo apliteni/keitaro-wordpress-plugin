46e693f2d64772fe05f7c01ecc71e27fe59ab873
Artur Sabirov <artur.sabirov@apliteni.com>
2019-11-12 14:37:35 +0100

    fix

--- ci/bin/svn_commit.sh
+++ ci/bin/svn_commit.sh
@@ -1,6 +1,6 @@
 set -e -x -o pipefail
 
 cd ./svn/
-svn co --username ${WP_USERNAME} '${WP_REPO}'
+svn co --username=${WP_USERNAME} "${WP_REPO}"
 svn add ./assets/* ./trunc/* 2>/dev/null
-svn ci -m "Release ${APP_VERSION}" --username ${WP_USERNAME} --password '${WP_PASSWORD}' --no-auth-cache
+svn ci -m "Release ${APP_VERSION}" --username ${WP_USERNAME} --password "${WP_PASSWORD}" --no-auth-cache
