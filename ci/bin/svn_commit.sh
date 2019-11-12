b56c37570d3af49d965ec161f06ad501eada38c8
Artur Sabirov <artur.sabirov@apliteni.com>
2019-11-12 14:56:59 +0100

    fix

--- ci/bin/svn_commit.sh
+++ ci/bin/svn_commit.sh
@@ -9,5 +9,5 @@ fi
 svn co --username=${WP_USERNAME} "${WP_REPO}"
 mv keitaro-tracker-integration/.svn ./svn/.svn
 cd ./svn/
-ls ./
-svn ci -m "Release ${APP_VERSION}" --username ${WP_USERNAME} --password "${WP_PASSWORD}" --no-auth-cache
+svn add ./assets/* ./trunc/* 2>/dev/null || true
+svn ci -m "Release ${APP_VERSION}" --username=${WP_USERNAME} --password="${WP_PASSWORD}" --no-auth-cache
