c80d0081898d72c617eea65352490c10c31c1f1b
Artur Sabirov <artur.sabirov@apliteni.com>
2019-11-12 16:15:05 +0100

    fix

--- ci/bin/svn_commit.sh
+++ ci/bin/svn_commit.sh
@@ -6,9 +6,10 @@ if [[ ! -v APP_VERSION ]]; then
 fi
 
 commit_msg="Release ${APP_VERSION}"
+password=$(echo "'${WP_PASSWORD}'")
 
 svn co --username=${WP_USERNAME} "${WP_REPO}"
 mv keitaro-tracker-integration/.svn ./svn/.svn
 cd ./svn/
 svn add ./assets/* ./trunc/* 2>/dev/null || true
-svn ci -m ${commit_msg} --username "${WP_USERNAME}" --password "${WP_PASSWORD}" --no-auth-cache
+svn ci -m ${commit_msg} --username "${WP_USERNAME}" --password ${password} --no-auth-cache
