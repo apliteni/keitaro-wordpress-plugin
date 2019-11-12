a9f5ec654e064fe8ee90c0d9733ebfcdbf332ea0
Artur Sabirov <artur.sabirov@apliteni.com>
2019-11-12 16:10:46 +0100

    fix

--- ci/bin/svn_commit.sh
+++ ci/bin/svn_commit.sh
@@ -5,8 +5,10 @@ if [[ ! -v APP_VERSION ]]; then
     echo "APP_VERSION is not set" && exit 1
 fi
 
+commit_msg="Release ${APP_VERSION}"
+
 svn co --username=${WP_USERNAME} "${WP_REPO}"
 mv keitaro-tracker-integration/.svn ./svn/.svn
 cd ./svn/
 svn add ./assets/* ./trunc/* 2>/dev/null || true
-svn ci -m "Release ${APP_VERSION}" --username "${WP_USERNAME}" --password "${WP_PASSWORD}" --no-auth-cache
+svn ci -m ${commit_msg} --username "${WP_USERNAME}" --password "${WP_PASSWORD}" --no-auth-cache
