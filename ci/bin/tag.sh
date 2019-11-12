aeb7043e6d0591079b7607e4303a4ce2b45bedb4
Artur Sabirov <artur.sabirov@apliteni.com>
2019-11-12 12:58:13 +0100

    fix

--- ci/bin/tag.sh
+++ ci/bin/tag.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e -x -o pipefail
 
-curl -X POST --show-error --fail "https://{GITLAB_URL}/api/v4/projects/${CI_PROJECT_ID}/repository/tags?ref=${CI_COMMIT_SHA}&tag_name=${APP_VERSION}&private_token=${GITLAB_TOKEN}"
+curl -X POST --show-error --fail "https://{CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/repository/tags?ref=${CI_COMMIT_SHA}&tag_name=${APP_VERSION}&private_token=${GITLAB_TOKEN}"
