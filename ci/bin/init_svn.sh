799268ac617ea9136f11ef16322d23b5da72dc1f
Artur Sabirov <artur.sabirov@apliteni.com>
2019-11-12 13:02:16 +0100

    fix

--- ci/bin/init_svn.sh
+++ ci/bin/init_svn.sh
@@ -1,3 +1,5 @@
+set -e -x -o pipefail
+
 mkdir -p svn/trunk
 mkdir -p svn/assets
 mkdir -p svn/tags
--- ci/bin/version.sh
+++ ci/bin/version.sh
@@ -1,1 +1,7 @@
 export APP_VERSION=$(cat VERSION)
+
+
+if [[ ! -v APP_VERSION ]]; then
+    echo "APP_VERSION is not set" && exit
+fi
+
