1672222962710868d6182248e0bcbe7d5c0d2336
Artur Sabirov <artur.sabirov@apliteni.com>
2019-11-12 13:03:33 +0100

    fix

--- ci/bin/tag.sh
+++ ci/bin/tag.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -e -x -o pipefail
 
+
+if [[ ! -v APP_VERSION ]]; then
+    echo "APP_VERSION is not set" && exit
+fi
+
 curl -X POST --show-error --fail "https://${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/repository/tags?ref=${CI_COMMIT_SHA}&tag_name=${APP_VERSION}&private_token=${GITLAB_TOKEN}"
