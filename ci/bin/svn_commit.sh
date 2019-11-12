ec7656388ed2405f2cacdf3952844da1208219cb
Artur Sabirov <artur.sabirov@apliteni.com>
2019-11-12 13:22:32 +0100

    fix

--- ci/bin/svn_commit.sh
+++ ci/bin/svn_commit.sh
@@ -1,5 +1,5 @@
 set -e -x -o pipefail
 
-cd ./svn/ && \
+cd ./svn/
 svn add ./assets/* ./trunc/* 2>/dev/null; true
 svn ci -m "Release ${APP_VERSION}" --username ${WP_USERNAME} --password ${WP_PASSWORD}
