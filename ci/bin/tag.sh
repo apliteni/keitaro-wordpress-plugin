8ccc6c60318469e861e240ba5aa015729ec77400
Ivan Roslov <ivan.roslov@apliteni.com>
2019-12-23 09:51:23 +0300

    KWP-8 add version to the scripts

--- ci/bin/tag.sh
+++ ci/bin/tag.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e -x -o pipefail
 
+export APP_VERSION=$(cat VERSION)
 
 if [[ ! -v APP_VERSION ]]; then
-    echo "APP_VERSION is not set" && exit
+    echo "APP_VERSION is not set" && exit 1
 fi
 
 curl -X POST --show-error --fail "https://${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/repository/tags?ref=${CI_COMMIT_SHA}&tag_name=${APP_VERSION}&private_token=${GITLAB_TOKEN}"
